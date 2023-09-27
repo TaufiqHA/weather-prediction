cabc271c5918cd9735dd0b6a3fc6f9d01dc6101b
Taufiq <htaufiq225@gmail.com>
2023-09-27 22:27:01 +0800

    add home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <h2>testing</h2>
+      <div className="container h-screen mx-auto py-5 flex gap-5">
+        <div className="card w-20 h-full bg-slate-700 shadow-xl box-content ">
+          <div className="card-body"></div>
+        </div>
+        <div className="content card flex-1 h-full bg-slate-700 shadow-xl box-content ">
+          <div className="card-body">
+            <div className="card w-full h-10 bg-base-100 shadow-xl px-5 flex justify-center ">
+              <h2>search...</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
